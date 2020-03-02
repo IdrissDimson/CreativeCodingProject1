abad1baf8e36e52286544d4b71de96c61683a25a
Idriss Dimson <idriss.dimson@gmail.com>
2020-03-02 11:22:04 -0500

    first saave

---
+++ mySketch.js
@@ -0,0 +1,17 @@
+let x, y, circleColor;
+function setup() {
+	createCanvas(windowWidth, windowHeight);
+	background(100);
+	circleColor = color(255, 0, 0);
+}
+
+
+function draw() {
+	// when the function completes it's animation go to the next one
+}
+
+function firstScene(speed) {
+	// The first scene of the project
+	
+	
+}
