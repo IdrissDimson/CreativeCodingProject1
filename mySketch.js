1fe887d8a92f20cbc00b4e9f40b196b4f3852cc3
Idriss Dimson <idriss.dimson@gmail.com>
2020-03-07 08:17:04 -0500

    added last week's progress

--- mySketch.js
+++ mySketch.js
@@ -1,17 +1,37 @@
 let x, y, circleColor;
-function setup() {
-	createCanvas(windowWidth, windowHeight);
-	background(100);
-	circleColor = color(255, 0, 0);
-}
+let particles = [];
 
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(100);
+  circleColor = color(255, 0, 0);
+  for (let i = 0; i < 20; i++) {
+    particles[i] = new Particle(5, 5, circleColor);
+  }
+}
 
 function draw() {
-	// when the function completes it's animation go to the next one
+  // when the function completes it's animation go to the next one
+  for (let i = 0; i < particles.length; i++) {
+    x = width / i;
+    y = height / i;
+    particles[i].display(random(0, 5));
+  }
 }
 
-function firstScene(speed) {
-	// The first scene of the project
-	
-	
+class Particle {
+  constructor(w, h, color) {
+    this.w = w;
+    this.h = h;
+    this.color = color;
+  }
+  update(speed) {
+    this.speedX += speed;
+    this.speedY += speed;
+  }
+  display() {
+    noStroke();
+    fill(this.color);
+    ellipse(x, y, this.w, this.h);
+  }
 }
